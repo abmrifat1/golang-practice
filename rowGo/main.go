19e9b29bcdbc93d7d676d4dcd3bf22c4d8082858
abmrifat1 <rifatabm1@gmail.com>
2021-03-09 15:59:17 +0600

    first commit.

---
+++ rowGo/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi go!!")
+
+	fmt.Println("pai"+"value"+" "+"is:", 3+.1416)
+
+	var a string = "rifat"
+
+	fmt.Println("my name"+" "+"is:", a)
+
+}
